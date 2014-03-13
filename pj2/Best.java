f89af55cb5f10ea314f5dbd9461ee593a981a773
sophiecooper <sophie@cooperzone.net>
2014-03-13 15:31:56 -0700

    Added pj2 files

---
+++ pj2/Best.java
@@ -0,0 +1,19 @@
+public class Best extends Move {
+
+
+	public int score;
+	public int x;
+	public int y;
+	public Move move;
+
+
+	public Best(int x, int y) {
+		move = super(x, y);
+	}
+
+
+
+
+
+
+}
